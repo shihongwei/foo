68f6bb683fd8688396df8c5428d4c7c8cff79012
时洪伟 <hongwei_shi@sina.cn>
2018-04-23 10:09:06 +0800

    Init project

---
+++ src/main/java/foo/FooApplication.java
@@ -0,0 +1,17 @@
+package foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: shihongwei
+ * @date: 2018-04-23 10:00
+ * @description:
+ */
+@SpringBootApplication
+public class FooApplication {
+  public static void main(String[] args) {
+    //
+    SpringApplication.run(FooApplication.class, args);
+  }
+}
